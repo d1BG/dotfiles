8fa9bcb23900c6d1037ab914581dde354b8a3d02
deni111BG <144280774+d1BG@users.noreply.github.com>
2024-01-18 18:33:26 +0200

    Add files via upload

--- DotFiles.sh
+++ DotFiles.sh
@@ -1,27 +1,59 @@
 #!/bin/bash
 
+# MangoHud
 
-#Neofetch Configs
+echo "Mangohud config: big includes all cores and other details, small does not"
+mkdir ~/.config/MangoHud
+read -p "Mangohud Config (big/small):" mangohudconf
 
+case $mangohudconf in
+big|BIG|Big )
+    cp mangohud/mangobig ~/.config/MangoHud/MangoHud.conf
+    ;;
+small|SMALL|Small )
+    cp mangohud/mangosmall ~/.config/MangoHud/MangoHud.conf
+    ;;
+* )
+    echo "Invaild response";
+    exit 1;;
+esac
 
-echo "Neofetch themes:"
+# Themes
+
+echo "Themes:"
 echo "[1] Nakiri"
 echo "[2] Raiden Shogun"
+echo "[3] Hutao"
+echo "[4] Navia"
+echo "[5] Frieren"
+echo "[6] Ninomae Ina'nis (colors r bugged)"
 
-read -p "Choose a theme: " neotheme
+read -p "Choose a theme: " theme
 
-echo "Copying theme files into ~/.config/neofetch"
-if [ $neotheme -eq 1 ]; then
+echo "Copying theme files into ~/.config/neofetch"   # I should have done those with in a case,
+                                                     # but i already did it the messy way and its
+                                                     # a personal script so I aint rewritting it,
+                                                     # its good enough
+if [ $theme -eq 1 ]; then
     cp neofetch/nakiri/* ~/.config/neofetch
-elif [ $neotheme -eq 2 ]; then
+elif [ $theme -eq 2 ]; then
     cp neofetch/raiden/* ~/.config/neofetch
+elif [ $theme -eq 3 ]; then
+    cp neofetch/hutao/* ~/.config/neofetch
+elif [ $theme -eq 4 ]; then
+    cp neofetch/navia/* ~/.config/neofetch
+elif [ $theme -eq 5 ]; then
+    cp neofetch/frieren/* ~/.config/neofetch
+elif [ $theme -eq 6 ]; then
+    cp neofetch/ninomae/* ~/.config/neofetch
 else
     echo "No such theme"
 fi
 
-# Kitty Configs
+# Kitty & Nano Configs
 
 cp kitty/kitty.conf ~/.config/kitty/kitty.conf
+cp nanorc ~/.zshrc
 
 #Zsh/OMZ Configs
 
@@ -48,17 +80,26 @@ n|N )
 esac
 
 echo "Copying theme files into ~/.oh-my-zsh/custom/themes/"
-rm ~/.oh-my-zsh/themes/fox.zsh-theme
-if [ $neotheme -eq 1 ]; then
-    cp omz/themes/red-fox.zsh-theme ~/.oh-my-zsh/custom/themes/fox.zsh-theme
-elif [ $neotheme -eq 2 ]; then
-    cp omz/themes/purple-fox.zsh-theme ~/.oh-my-zsh/custom/themes/fox.zsh-theme
+if [ $theme -eq 1 ]; then
+    cp omz/themes/red-fox.zsh-theme ~/.oh-my-zsh/custom/themes/custom-fox.zsh-theme
+elif [ $theme -eq 2 ]; then
+    cp omz/themes/purple-fox.zsh-theme ~/.oh-my-zsh/custom/themes/custom-fox.zsh-theme
+elif [ $theme -eq 3 ]; then
+    cp omz/themes/red-fox.zsh-theme ~/.oh-my-zsh/custom/themes/custom-fox.zsh-theme
+elif [ $theme -eq 4 ]; then
+    cp omz/themes/navia-fox.zsh-theme ~/.oh-my-zsh/custom/themes/custom-fox.zsh-theme
+elif [ $theme -eq 5 ]; then
+    cp omz/themes/yellow-fox.zsh-theme ~/.oh-my-zsh/custom/themes/custom-fox.zsh-theme
+elif [ $theme -eq 6 ]; then
+    cp omz/themes/purple-fox.zsh-theme ~/.oh-my-zsh/custom/themes/custom-fox.zsh-theme
 fi
 
 echo "Copying zshrc file"
+cp ~/.zshrc ~/.zshrc.before-d1bg-dots.bak
 cp zshrc ~/.zshrc
 
-echo "Copying plugins"
+
+echo "Copying omz plugins"
 cp omz/plugins/plugins.zip ~/.oh-my-zsh/custom/plugins/
 cd ~/.oh-my-zsh/custom/plugins/
 unzip -qq plugins.zip
